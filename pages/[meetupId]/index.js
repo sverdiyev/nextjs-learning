ddf493c22283d9e9e669a08686b6c8983973e257
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2021-12-25 15:35:55 +0100

    cleaned-up

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,25 +4,6 @@ import React from 'react';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 import Head from 'next/head';
 
-const Dummy_Meetups = [
-  {
-    id: 'm1',
-    title: 'first meetup',
-    image:
-      'https://images.unsplash.com/photo-1640210178824-d029d3cfd676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80',
-    address: 'lviv',
-    description: 'describe me ',
-  },
-  {
-    id: 'm2',
-    title: 'first meetup',
-    image:
-      'https://images.unsplash.com/photo-1640210178824-d029d3cfd676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80',
-    address: 'lviv',
-    description: 'describe me ',
-  },
-];
-
 function MeetupDetails(props) {
   const { meetup } = props;
 
@@ -70,8 +51,6 @@ export async function getStaticPaths() {
   return { fallback: true, paths: meetupsId };
 }
 
-// export async function getStaticProps({ params: {slug} }) {
-
 export async function getStaticProps(context) {
   const password = 'aUSjJhO7eUkOOC1t';
   const username = 'learningnext';
@@ -87,7 +66,6 @@ export async function getStaticProps(context) {
   const loadedMeetup = await meetupsCollection.findOne({
     _id: ObjectId(meetupId),
   });
-  console.log('🚀 ~ getStaticProps ~ loadedMeetup', loadedMeetup);
 
   client.close();
 
